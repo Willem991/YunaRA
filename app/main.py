47f0ee489950a373baa7f44448f9656aa0b27665
Willem Nieuwenhuis <ruan.nieu@gmail.com>
2026-09-03 14:25:11 +0200

    Added some icons

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ import sys
 
 from PyQt6.QtWidgets import QApplication
 from app.gui.screens.main_window import MainWindow
+from PyQt6.QtGui import QIcon
 
 app = QApplication(sys.argv)
+app.setWindowIcon(QIcon("app/resources/images/yunara.png"))
 
 with open("./app/resources/styles/main.qss", "r") as file:
     app.setStyleSheet(file.read())
 
 window = MainWindow()
+window.setWindowTitle("YunaRA")
 window.show()
 
 app.exec()
